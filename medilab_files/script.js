0775ca2a396b1ec06887aa5a5cd06770f8b4fc2b
proxy6 <progresseze@gmail.com>
2020-06-30 20:07:26 +0100

    fixed

--- medilab_files/script.js
+++
@@ -1,9 +0,0 @@
-
-let open = false
-const navToggle = document.querySelector('.navbar-toggler');
-const nav = document.querySelector('.navbar');
-navToggle.addEventListener('click', () => {
-  if(!open) {
-    nav.classList.add('nav-sticky');
-  } 
-});
